5815a53c5de79207cfd41d3b4531e6cce20b2071
Deepansh0007 <you@example.com>
2023-08-05 13:21:43 +0530

    added projects

--- components/About.jsx
+++ components/About.jsx
@@ -13,11 +13,11 @@ const About = () => {
         <p data-aos= "fade-up" className='mt-10 md:text-xl md:px-10 px-5'>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
     </div>
     <div className='grid md:grid-cols-2 md:gap-y-5 md:px-10 px-5'>
-        <div data-aos="zoom-in-right" className='md:mt-10 mt-5'>
+        <div data-aos="fade-right" className='md:mt-10 mt-5'>
         <h2 className='text-2xl font-bold'>Get to know me!</h2>
         <p className='md:text-lg mt-5 md:10 leading-7'>I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the <span className='font-bold'>Projects</span> section.I'm open to Job opportunities where I can contribute, learn and grow. <br/>If you have a good opportunity that matches my skills and experience then don't hesitate to <a href='#contact' className="text-blue-600 font-semibold">contact</a> me.</p>
         </div>
-        <div data-aos="zoom-in-left" className='md:pl-10 md:mt-10 mt-5'>
+        <div data-aos="fade-right" className='md:pl-10 md:mt-10 mt-5'>
         <h2 className='text-2xl font-bold'>My skills</h2>
         <div className='flex md:mt-10 mt-5 md:gap-3 gap-2 flex-wrap'>
             <Card text = "HTML"/>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import metaverse from "../public/metaverse.png";
 
-const Projects = () => {
+const Project = ({ title, description, image, link }) => {
   return (
-    <div id = "projects" className='bg-slate-50'>
-         <div className='flex items-center justify-center flex-col'>
-        <h1 data-aos="zoom-in" className='text-3xl font-bold'>PROJECTS</h1>
+      <div className="grid lg:grid-cols-2 my-10 lg:gap-10">
+        <div data-aos = "fade-right" className="h-full w-full">
+          <Image src = {image} alt = "project"/>
         </div>
-    </div>
-  )
-}
+        <div data-aos = "fade-right" className="lg:mt-10 mt-5">
+          <h1 className="font-semibold text-3xl lg:text-left text-center">{title}</h1>
+          <div className="leading-8 text-lg my-8">{description}</div>
+          <a href={link} className="px-8 bg-primary rounded-xl text-white py-3">Live link</a>
+        </div>
+      </div>
+  );
+};
+const Projects = () => {
+	return (
+		<div id="projects" className="bg-slate-50 lg:p-10 p-5">
+			<div className="flex items-center justify-center flex-col">
+				<h1 data-aos="zoom-in" className="text-3xl font-bold">
+					PROJECTS
+				</h1>
+				<Project title="Metaverse" image={metaverse} description="fdscbsadkverfkkfvbaukrbkvbauhtf tf tyyyyyyyyy yyyyy yyyyyyyy yycdtydtyhhhhhhhhh
+        hhhhhhhhhhhhhhhhhhhhhhhzzzzzh kuhuibgigyy ugyugbyufoyy ygygyofyfyf iuguing8norf" link= "https://multiversemadness.netlify.app" />
+        <Project title="Metaverse" image={metaverse} description="fdscbsadkverfkkfvbaukrbkvbauhtf tf tyyyyyyyyy yyyyy yyyyyyyy yycdtydtyhhhhhhhhh
+        hhhhhhhhhhhhhhhhhhhhhhhzzzzzh kuhuibgigyy ugyugbyufoyy ygygyofyfyf iuguing8norf" link= "https://multiversemadness.netlify.app" />
+        <Project title="Metaverse" image={metaverse} description="fdscbsadkverfkkfvbaukrbkvbauhtf tf tyyyyyyyyy yyyyy yyyyyyyy yycdtydtyhhhhhhhhh
+        hhhhhhhhhhhhhhhhhhhhhhhzzzzzh kuhuibgigyy ugyugbyufoyy ygygyofyfyf iuguing8norf" link= "https://multiversemadness.netlify.app" />
+			</div>
+		</div>
+	);
+};
 
-export default Projects
+export default Projects;
