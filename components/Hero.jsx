2c15a1e9fa2e8e694ed4ccaf61e8b2eaee21f349
Deepansh0007 <you@example.com>
2023-08-07 21:09:22 +0530

    Fixed background

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div data-aos = "fade-up" className='flex justify-center place-items-center flex-col min-h-screen px-10 hero_main'>
+    <div data-aos = "fade-up" className='flex justify-center place-items-center flex-col min-h-screen px-10'>
         <h1 className="text-5xl font-extrabold tracking-wider">HEY I'AM DEEPANSH</h1>
         <h1 className="text-5xl font-extrabold tracking-wide mt-5">I'AM A FULL STACK DEVELOPER</h1>
         
