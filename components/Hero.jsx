f8a716cf10c4f49cffcafaafd169e092a6eb89a6
Deepansh0007 <you@example.com>
2023-09-19 21:17:52 +0530

    re-design

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div data-aos = "fade-up" className='flex justify-center place-items-center flex-col min-h-screen px-10 bg text-center text-white'>
+    <div className='flex justify-center place-items-center flex-col min-h-screen px-10 bg text-center text-white'>
         <h1 className="text-5xl font-extrabold tracking-wider">HEY I'AM DEEPANSH</h1>
         <h1 className="text-5xl font-extrabold tracking-wide mt-5">I'AM A FULL STACK DEVELOPER</h1>
         
